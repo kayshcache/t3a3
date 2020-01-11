14c361e1aded96ff26309cd6eb4c406f4a38c888
Kaysh <29114402+kayshcache@users.noreply.github.com>
2020-01-11 12:37:06 +1100

    Create higherOrder.ts

---
+++ higherOrder.ts
@@ -0,0 +1,13 @@
+// Higher order functions returning another function
+// Must use somehow, get it in play
+const higherOrder = n => {
+  const doSomething = m => {
+    const doWhatEver = t => {
+      return 2 * n + 3 * m + t
+    }
+    return doWhatEver
+  }
+​
+  return doSomething
+}
+console.log(higherOrder(2)(3)(10))
